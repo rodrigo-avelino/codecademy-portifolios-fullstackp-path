a44dafda6ec93086425142a1126369bcd36dcf67
Rodrigo Avelino <rodrigo.avelino22@gmail.com>
2021-06-25 16:27:09 -0300

    added data to the proect

---
+++ script.js
@@ -0,0 +1,20 @@
+//bibliotecas de frases
+const frases_do_dia = [
+    'com ou sem luta, sempre ha derrota',
+    'calma, voce ainda tem muito a perder',
+    'quando te falarem que nao vai da certo, acredite',
+    'na vida sempre  e tudo ou nada. tudo dando errado ou nada dando certo',
+    'dias ruins se passam para que piores possam vir',
+    'nunca foi azar, sempre foi incompetencia',
+    'depois da tempestade vem o lamacal',
+    'ate o cheque e espepcial e voce nao'
+];
+const mood_do_dia = [
+    'suicidio',
+    'rejeicao amorosa',
+    'deprpessao',
+    'fracasso profissional',
+    'desespero',
+    'vontade de morrer'
+];
+//funcoes
